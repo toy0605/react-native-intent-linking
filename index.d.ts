55778f15b712595bea87b5f6fbc1be8130b91641
이승렬 <toy0605@gmail.com>
2020-06-26 19:16:19 +0900

    Add declaration module.

--- index.d.ts
+++ index.d.ts
@@ -1,4 +1,6 @@
 
-declare function openURL(url: string): Promise<void>;
-declare function canOpenURL(url: string): Promise<boolean>;
-declare function getPackageName(url: string): Promise<string | null>;
+declare module "react-native-intent-linking" {
+    declare function openURL(url: string): Promise<void>;
+    declare function canOpenURL(url: string): Promise<boolean>;
+    declare function getPackageName(url: string): Promise<string | null>;
+}

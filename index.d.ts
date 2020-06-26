e8dc988b71b9818024aba32a76b12a95d4007f1a
이승렬 <toy0605@gmail.com>
2020-06-26 19:18:49 +0900

    Fix syntax error

--- index.d.ts
+++ index.d.ts
@@ -1,6 +1,6 @@
 
 declare module "react-native-intent-linking" {
-    declare function openURL(url: string): Promise<void>;
-    declare function canOpenURL(url: string): Promise<boolean>;
-    declare function getPackageName(url: string): Promise<string | null>;
+    function openURL(url: string): Promise<void>;
+    function canOpenURL(url: string): Promise<boolean>;
+    function getPackageName(url: string): Promise<string | null>;
 }
